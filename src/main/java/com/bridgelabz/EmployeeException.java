01ba0c3ab8ad658a9bc7c3ce50c8d96fe3b02f80
Aniket1675 <Aniket@05>
2022-04-19 12:47:38 +0530

    ADDED:Program To retrive data from payroll service database.

---
+++ src/main/java/com/bridgelabz/EmployeeException.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+/**
+ * Purpose: Class for Exception Handling.
+ *
+ * @author Aniket Kumbhar
+ * @version 1.0
+ * @since 18-03-2022
+ */
+public class EmployeeException extends RuntimeException{
+
+    public EmployeeException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/bridgelabz/EmployeePayRoll.java
+++ src/main/java/com/bridgelabz/EmployeePayRoll.java
@@ -1,9 +1,53 @@
 package com.bridgelabz;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Purpose: Class Employee Payroll.
+ *
+ * @author Aniket Kumbhar
+ * @version 1.0
+ * @since 18-03-2022
+ *
+ */
+
 public class EmployeePayRoll {
     public static void main(String[] args) {
-        System.out.println("Welcome to JDBC program");
+        String FETCH = "SELECT * FROM employee_payroll";
+        ArrayList<Employee> empList = new ArrayList<Employee>();
         EmployeeConfig eConfig = new EmployeeConfig();
         eConfig.getConfig();
+        PreparedStatement preparedStatement;
+        try {
+            preparedStatement = eConfig.getConfig().prepareStatement(FETCH);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Employee employee = new Employee();
+
+                employee.setEmpId(resultSet.getInt("id"));
+                employee.setEmpName(resultSet.getString("name"));
+                employee.setPhoneNumber(resultSet.getString("Salary"));
+                employee.setEmpStart(resultSet.getString("Start_Date"));
+                employee.setGender(resultSet.getString("Gender"));
+                employee.setPhoneNumber(resultSet.getString("Phone"));
+                employee.setAddress(resultSet.getString("Address"));
+                employee.setBasicPay(resultSet.getDouble("Basic_Pay"));
+                employee.setDeductions(resultSet.getDouble("Deductions"));
+                employee.setTaxablePay(resultSet.getDouble("Taxable_Pay"));
+                employee.setIncomeTax(resultSet.getDouble("Income_Tax"));
+                employee.setNetPay(resultSet.getDouble("Net_Pay"));
+
+                empList.add(employee);
+            }
+            for (Employee i : empList) {
+                System.out.println(i.toString());
+            }
+        }
+        catch (SQLException e) {
+            throw new EmployeeException("invalid column label");
+        }
     }
 }
