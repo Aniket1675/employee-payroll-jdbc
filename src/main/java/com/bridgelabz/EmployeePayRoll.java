238dece0999dd346153edec8d400aca6f5ec735e
Aniket1675 <Aniket@05>
2022-04-19 12:13:14 +0530

    ADDED: Creating database in Mysql Workbench and connection database using JDBC program.

--- src/main/java/com/bridgelabz/EmployeePayRoll.java
+++ src/main/java/com/bridgelabz/EmployeePayRoll.java
@@ -3,5 +3,7 @@ package com.bridgelabz;
 public class EmployeePayRoll {
     public static void main(String[] args) {
         System.out.println("Welcome to JDBC program");
+        EmployeeConfig eConfig = new EmployeeConfig();
+        eConfig.getConfig();
     }
 }
